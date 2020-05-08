677dbb4823220712161ebe434b0c0fe694b0ffdf
root <root@fermicloud157.fnal.gov>
2020-05-08 01:00:42 -0500

    purging run folder

--- docker/xrootd-standalone_r/run-it.sh
+++ docker/xrootd-standalone_r/run-it.sh
@@ -1,11 +1,10 @@
 echo "Rmember to: \ncreate xrd and copy cert/key there\ncreate ligo user\n create voms-mapfile"
 docker run -it --entrypoint "/bin/bash" \
     --publish 1094:1094 \
-    --volume $(pwd)/10-my-site-variables.cfg:/etc/xrootd/config.d/10-common-site-local.cfg \
+    --volume $(pwd)/my_xrootd-standalone.cfg:/etc/xrootd/xrootd-standalone.cfg \
     --volume $(pwd)/my_auth_file:/etc/xrootd/auth_file \
-    --volume $(pwd)/90-my-paths.cfg:/etc/xrootd/config.d/90-osg-standalone-paths.cfg \
     --volume $(pwd)/mygridmapfile:/etc/grid-security/grid-mapfile \
-    --volume /tmp/to_export/:/data \
+    --volume /data/:/data \
     --volume /etc/grid-security/hostcert.pem:/etc/grid-security/hostcert.pem \
     --volume /etc/grid-security/hostkey.pem:/etc/grid-security/hostkey.pem \
-    opensciencegrid/xrootd-standalone:fresh
+    --name xrootd_standalone opensciencegrid/xrootd-standalone:fresh
--- docker/xrootd-standalone_r/run.sh
+++ docker/xrootd-standalone_r/run.sh
@@ -1,10 +1,10 @@
 docker run --rm \
     --publish 1094:1094 \
-    --volume $(pwd)/10-my-site-variables.cfg:/etc/xrootd/config.d/10-common-site-local.cfg \
+    --volume $(pwd)/my_xrootd-standalone.cfg:/etc/xrootd/xrootd-standalone.cfg \
     --volume $(pwd)/my_auth_file:/etc/xrootd/auth_file \
-    --volume $(pwd)/90-my-paths.cfg:/etc/xrootd/config.d/90-osg-standalone-paths.cfg \
     --volume $(pwd)/mygridmapfile:/etc/grid-security/grid-mapfile \
-    --volume /tmp/to_export/:/data \
+    --volume $(pwd)/data/:/data/ \
     --volume /etc/grid-security/hostcert.pem:/etc/grid-security/hostcert.pem \
     --volume /etc/grid-security/hostkey.pem:/etc/grid-security/hostkey.pem \
-    --name xrootd_standalone opensciencegrid/xrootd-standalone:fresh
+    --name xrootd_standalone opensciencegrid/xrootd-standalone:fresh \
+ 	&> /dev/null & 
