c502bc2ed124e1634fff7cf1969b0feab42fa6a8
Diego Davila <davila.foyo@gmail.com>
2020-05-08 14:54:21 -0500

    adding monitoring directive

--- docker/xrootd-standalone_r/run.sh
+++ docker/xrootd-standalone_r/run.sh
@@ -1,10 +1,10 @@
 docker run --rm \
     --publish 1094:1094 \
+    --publish 9930:9930 \
     --volume $(pwd)/my_xrootd-standalone.cfg:/etc/xrootd/xrootd-standalone.cfg \
     --volume $(pwd)/my_auth_file:/etc/xrootd/auth_file \
     --volume $(pwd)/mygridmapfile:/etc/grid-security/grid-mapfile \
     --volume $(pwd)/data/:/data/ \
     --volume /etc/grid-security/hostcert.pem:/etc/grid-security/hostcert.pem \
     --volume /etc/grid-security/hostkey.pem:/etc/grid-security/hostkey.pem \
-    --name xrootd_standalone opensciencegrid/xrootd-standalone:fresh \
- 	&> /dev/null & 
+    opensciencegrid/xrootd-standalone:fresh &> /dev/null &
